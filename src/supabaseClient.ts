a490b10e8cff6cefd93152acd4fb55fbbd982a05
jakubminka <jakubminka@gmail.com>
2026-02-05 19:46:51 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ysnwurduyfgymxtcfnhp.supabase.co';
-const supabaseKey = 'sb_publishable_Gfkjx3M6WQxLaEwk0ZE85w_yCpeRttj';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzbnd1cmR1eWZneW14dGNmbmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMTI4MTQsImV4cCI6MjA4NTg4ODgxNH0.WEJ5M1hZ4kGKnfmWo4NXImYVhHIynmmnrkbwxz0sBdQ';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
